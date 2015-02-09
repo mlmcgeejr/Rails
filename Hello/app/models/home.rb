9bd3919bd3a8408cf601362f48640d378fbdb253
mlmcgeejr <larry@mcgeemail.com>
2015-02-09 01:31:02 +0000

    I added my age and made some other corrections

--- Hello/app/models/home.rb
+++ Hello/app/models/home.rb
@@ -8,4 +8,18 @@ class Home
     birthdate = Date.new(1953,5,6)
   end
   
+  def yrsold
+    require 'date'
+    today = Date.today
+    #setup a date for my birthday this year
+    birthdate_thisyr = Date.new(today.year, birthdate.month, birthdate.day)
+    yrsold = today.year.to_i - birthdate.year.to_i
+    if birthdate_thisyr > today
+      yrsold = yrsold - 1
+    end
+    
+  end  
+    
+   
+ 
 end  
